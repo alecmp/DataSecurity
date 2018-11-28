de569b7add4532b69b9d8184c9ff8cdef5e1f4a6
alessandro.campanell <alessandro@ggg.com>
2018-11-28 08:38:46 +0100

    Fixed scanner crash

--- app/src/main/java/alessandro/datasecurity/activities/encrypt/EncryptActivity.java
+++ app/src/main/java/alessandro/datasecurity/activities/encrypt/EncryptActivity.java
@@ -22,15 +22,19 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
+import alessandro.datasecurity.MainActivity;
 import alessandro.datasecurity.activities.stego.StegoActivity;
 import alessandro.datasecurity.utils.Constants;
 import alessandro.datasecurity.utils.StandardMethods;
@@ -181,6 +185,34 @@ public class EncryptActivity extends AppCompatActivity implements EncryptView {
       actionBar.setTitle("Encryption");
     }
   }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.encryption_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_send) {
+      Toast.makeText(this, "Action clicked", Toast.LENGTH_LONG).show();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+
+
+
 
   @Override
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
